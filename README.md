a76156dccab15c148d516abb8a610451447ef825
Pablo Souza <pablo.osouza@outlook.com>
2023-04-14 07:14:39 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NeedleWork
+
+NeedleWork is an API developed in Kotlin designed especially for small embroidery businesses. Register what you sell and what you buy so you can keep track of your money flow with no surprises at the end.
---
+++ src/test/kotlin/br/com/verdebordo/needlework/NeedleWorkApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.verdebordo.needlework
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NeedleWorkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
